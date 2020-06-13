764ce0f0ed62d02072fe193a157e9e9816f5454f
Aman Gupta <aman@keio.jp>
2020-06-14 00:39:36 +0900

    clean up

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("/Users/aman/workspace/misaki/docs/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/aman/workspace/misaki/docs/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/aman/workspace/misaki/docs/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/aman/workspace/misaki/docs/node_modules/@docusaurus/theme-classic/src/prism-include-languages"),
+  require("/Users/aman/workspace/misaki/docs/src/css/custom.css"),
+];

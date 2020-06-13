bbf9ae6049cd0d350eeff0faf20e56dde28cd476
Aman Gupta <aman@keio.jp>
2020-06-14 00:41:40 +0900

    renaminf urls in config

---
+++ .docusaurus/docusaurus.config.js
@@ -0,0 +1,107 @@
+export default {
+  "plugins": [],
+  "themes": [],
+  "customFields": {},
+  "themeConfig": {
+    "navbar": {
+      "title": "Misaki",
+      "logo": {
+        "alt": "My Site Logo",
+        "src": "img/logo.svg"
+      },
+      "links": [
+        {
+          "to": "docs/",
+          "activeBasePath": "docs",
+          "label": "Docs",
+          "position": "left"
+        },
+        {
+          "to": "blog",
+          "label": "Blog",
+          "position": "left"
+        },
+        {
+          "href": "https://github.com/misaki-io",
+          "label": "GitHub",
+          "position": "right"
+        }
+      ]
+    },
+    "footer": {
+      "style": "dark",
+      "links": [
+        {
+          "title": "Docs",
+          "items": [
+            {
+              "label": "Style Guide",
+              "to": "docs/"
+            },
+            {
+              "label": "Second Doc",
+              "to": "docs/doc2/"
+            }
+          ]
+        },
+        {
+          "title": "Community",
+          "items": [
+            {
+              "label": "Stack Overflow",
+              "href": "#"
+            },
+            {
+              "label": "Discord",
+              "href": "#"
+            },
+            {
+              "label": "Twitter",
+              "href": "https://twitter.com/syilvr"
+            }
+          ]
+        },
+        {
+          "title": "More",
+          "items": [
+            {
+              "label": "Blog",
+              "to": "blog"
+            },
+            {
+              "label": "GitHub",
+              "href": "https://github.com/misaki-io"
+            }
+          ]
+        }
+      ],
+      "copyright": "Copyright © 2020 My Project, Inc. Built with Docusaurus."
+    }
+  },
+  "title": "Misaki",
+  "tagline": "Deploy anywhere",
+  "url": "https://misaki.io",
+  "baseUrl": "/docs/",
+  "favicon": "img/favicon.ico",
+  "organizationName": "misaki-io",
+  "projectName": "docs",
+  "presets": [
+    [
+      "@docusaurus/preset-classic",
+      {
+        "docs": {
+          "homePageId": "doc1",
+          "sidebarPath": "/Users/aman/workspace/misaki/docs/sidebars.js",
+          "editUrl": "https://github.com/misaki-io/docs/"
+        },
+        "blog": {
+          "showReadingTime": true,
+          "editUrl": "https://github.com/misaki-io/docs/"
+        },
+        "theme": {
+          "customCss": "/Users/aman/workspace/misaki/docs/src/css/custom.css"
+        }
+      }
+    ]
+  ]
+};
